75639e22f77801be1a34652fa77311b157be8a60
Shivama Thakur     /IT/ICICILOMBARD/VIKH <1042257@icicilombard.com>
2024-10-04 15:19:43 +0530

    Added Stuff

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        // GET ALL : https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //var regions = new List<Region>
+            //{
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Wellington Region",
+            //        Code = "WLG",
+            //        RegionImageUrl = "https://media.istockphoto.com/id/464378599/photo/wellington-bay-and-harbour.jpg?s=612x612&w=0&k=20&c=XDTrWiAHhiltcbG2cd6e9j78UxWTWiKjFB0vOnZmfA0="
+            //    },
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Auckland Region",
+            //        Code = "AKL",
+            //        RegionImageUrl = "https://cdn.britannica.com/99/61399-050-B867F67F/skyline-Auckland-New-Zealand-Westhaven-Marina.jpg"
+            //    }
+            //};
+            var regions = _dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        // GET BY ID : https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id) {
+            //var region = _dbContext.Regions.Find(id);  //Works only with primary key (here, id)
+            var region = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(region);
+            }
+        }
+    }
+}
---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET : https://localhost:portno/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "A", "B", "C", "D" };
+            return Ok(studentNames);
+        }
+    }
+}
