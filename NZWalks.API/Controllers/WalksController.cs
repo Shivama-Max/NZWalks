63e0e39db83701c0429e72179caecc042618cffe
Shivama Thakur     /IT/ICICILOMBARD/VIKH <1042257@icicilombard.com>
2024-10-10 11:57:15 +0530

    Filtering,Sorting,Pagination

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -41,11 +41,14 @@ namespace NZWalks.API.Controllers
         }
 
         //READ WALKS
-        //GET
+        //GET : /api/walks?filterOn=Name&filterQuery=Track&sortBy=abcd&isAscending=True&pageNumber=1&pageSize=5
+        //Filtering is applied on GET only
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery
+            , [FromQuery] string? sortBy, [FromQuery] bool? isAscending
+            , [FromQuery] int pageNumber=1, [FromQuery] int pageSize=10)
         {
-            var walkDomain = await walkRepository.GetAllAsync();
+            var walkDomain = await walkRepository.GetAllAsync(filterOn,filterQuery,sortBy,isAscending ?? true,pageNumber,pageSize);
             return Ok(mapper.Map<List<WalkDTO>>(walkDomain));
         }
 
--- NZWalks.API/Repositories/SQLWalkRepository.cs
+++ NZWalks.API/Repositories/SQLWalkRepository.cs
@@ -20,9 +20,41 @@ namespace NZWalks.API.Repositories
             return walk;
         }
 
-        public async Task<List<Walk>> GetAllAsync()
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null
+            , string? sortBy = null, bool isAscending = true
+            , int pageNumber = 1, int pageSize = 10)
         {
-            return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
+            //return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
+
+            //To apply Sorting, Filtering & Pagination
+            var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
+
+            //Filtering
+            if(!string.IsNullOrWhiteSpace(filterOn) && !string.IsNullOrWhiteSpace(filterQuery))
+            {
+                if (filterOn.Equals("Name",StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = walks.Where(x=>x.Name.Contains(filterQuery));
+                }
+            }
+
+            //Sorting
+            if(!string.IsNullOrWhiteSpace(sortBy))
+            {
+                if(sortBy.Equals("Name",StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x=>x.Name) : walks.OrderByDescending(x=>x.Name);
+                }
+                if (sortBy.Equals("Length", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.LengthInKm) : walks.OrderByDescending(x => x.LengthInKm);
+                }
+            }
+
+            //Pagination
+            var skipResults = (pageNumber - 1) * pageSize;
+
+            return await walks.Skip(skipResults).Take(pageSize).ToListAsync();
         }
 
         public async Task<Walk?> GetByIdAsync(Guid id)
